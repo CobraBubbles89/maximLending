cc50a2905c9e2722626a6b2643cfea39e848f122
Yuriy_yan <vi060489jjv@gmail.com>
2025-05-05 12:22:21 +0300

    first version

---
+++ script.js
@@ -0,0 +1,83 @@
+
+
+class Header {
+
+    selectors = {
+        root: '.header',
+        overlay: '.header_overlay',
+        burgerButton: '.header__burger-button'
+    }
+
+    stateClasses = {
+        isActive: 'is-active',
+        isLock: 'is-lock'
+    }
+
+    constructor() {
+        this.rootElement = document.querySelector(this.selectors.root),
+        this.overlayElement = this.rootElement.querySelector(this.selectors.overlay),
+        this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton),
+        this.bindEvents()
+    }
+
+    onBurgerButtonClick = () => {
+        this.burgerButtonElement.classList.toggle(this.stateClasses.isActive)
+        
+        this.overlayElement.classList.toggle(this.stateClasses.isActive)
+        document.documentElement.classList.toggle(this.stateClasses.isLock)
+    }
+
+    bindEvents() {
+        this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
+    }
+
+}
+
+new Header();
+
+function popupInteractive() {
+    const popupBtn = document.querySelector('.header_contacts');
+    const popupCloser = document.querySelector('.popup_closer');
+    const overlay = document.querySelector('.popup_contacts_overlay');
+
+    const togglepopup = (show) => {
+        overlay.classList.toggle('is-active', show);
+        document.body.style.overflow = show ? 'hidden' : ''
+    }
+
+    popupBtn.addEventListener('click', () => { togglepopup(true) })
+
+    popupCloser.addEventListener('click', () => { togglepopup(false) })
+
+    overlay.addEventListener('click', (e) => {
+        if(e.target === overlay) { togglepopup(false) }
+    })
+
+    document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') { togglepopup(false) }
+    });
+}
+
+popupInteractive()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
